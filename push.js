9e9499351fb694d69b3939414032503e67acd229
aravindmannam <40652756+aravindmannam@users.noreply.github.com>
2020-05-06 11:42:38 +0530

    changes

--- push.js
+++ push.js
@@ -1,11 +1,11 @@
  var push=require('web-push');
- let vapdKeys={
-    publicKey:'BA1ZbV00nlYC-0n_vAnIY5vEhDL1NTRl3LFo82YAqpJ9HGdgmwLkoOhiTebZ79TJezNkzsQh3xEufCanJDJzURo',
-    privateKey: 'XbfzcO1n_nwFPZ8NniVnd2Fbj1Usd-I62yVkBOoDU5s'
+let vapdKeys = {
+    publicKey:
+        'BLhPynKRNMC-b_23FQERW-D2nYvn6BHCm5ZRibinoA4TmDtyUBcv1Dheo_2Bk68K759mdPTalDz-nRsZw3Q-Csw',
+    privateKey: 'TXUopwLk_HO9QSgy9u7DI1Hb_yHiwSFQvg7opB-Pl0Q'
 }
-
 push.setVapidDetails('mailto:aravindmannam24@gmail.com',vapdKeys.publicKey,vapdKeys.privateKey)
 
 let sub={}
 
-push.sendNotification(sub, 'text message')
+push.sendNotification(sub, 'test message')
