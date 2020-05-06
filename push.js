23b8d595684a82e1339af6abd2d3f545a0fccc21
aravindmannam <40652756+aravindmannam@users.noreply.github.com>
2020-05-06 12:08:01 +0530

    changes

--- push.js
+++ push.js
@@ -6,6 +6,14 @@ let vapdKeys = {
 }
 push.setVapidDetails('mailto:aravindmannam24@gmail.com',vapdKeys.publicKey,vapdKeys.privateKey)
 
-let sub={}
+let sub={
+    "endpoint": "https://fcm.googleapis.com/fcm/send/fq08nT9Uhyk:APA91bFlYTsrBV_btOPkKTdUmPBuAosYL9k0vYblrS8llMHn1lxukeBru3wO3iitet7YwRD7Dbhqr4dG3IMDUM45VjEOU8BXLGg_6ERq7EvaMC0_MxRyF0fWdEpkxXd00Mtp_7l-x1MK",
+     "expirationTime": null, 
+     "keys": 
+     { "p256dh": "BIs5RyaILUXKcX159MBHZus9jAeUi4Tf_9NV2eIpm8CMAbuDgxmKlL8A7WbEKyk4YQ2gWK8ilLbsDYcF0fOQDes", 
+     "auth": "LCHSJDacGfjemlT7129NxA" 
+    }
+     }
+
 
 push.sendNotification(sub, 'test message')
